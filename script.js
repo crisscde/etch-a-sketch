cdf136c8f68269a17585d8e13b35d269050fb047
Cristopher Cuautzo <cristophercuautzo@gmail.com>
2024-05-08 09:06:20 -0600

    Fix: Each time I create a new sketch, the first one is manteined

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-let body = document.querySelector("body");
 let sketch = document.getElementById("sketch");
 let button = document.getElementById("number");
 let numberSquare = document.getElementById("squares");
 
 function createSquares(numberOfSquares) {
+	if (sketch.childNodes.length > 0) {
+		while (sketch.firstChild) {
+			sketch.removeChild(sketch.firstChild);
+		}
+	}
 	let createdSquares = 0;
 	while (createdSquares < numberOfSquares * numberOfSquares) {
 		let square = document.createElement("div");
 		square.setAttribute("class", "square");
+		square.setAttribute(
+			"style",
+			`width: ${898 / numberOfSquares}px; height: ${468 / numberOfSquares}px`
+		);
 		sketch.appendChild(square);
-		// document.documentElement.style.setProperty("--bg-color", numberOfSquares);
+		createdSquares++;
 	}
-	createdSquares++;
 }
 
 button.addEventListener("click", (ev) => {
 	ev.preventDefault();
-	createSquares(numberSquare.value);
+	createSquares(Number(numberSquare.value));
 });
