7143449a21f28a0554534ddb10ca7508779dc9a9
Cristopher Cuautzo <cristophercuautzo@gmail.com>
2024-05-08 11:58:14 -0600

    Refactor createSquares() function

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let sketch = document.getElementById("sketch");
 let button = document.getElementById("number");
 let numberSquare = document.getElementById("squares");
 
+let newSquare = (totalSquares) => {
+	let square = document.createElement("div");
+	square.setAttribute("class", "square");
+	square.setAttribute(
+		"style",
+		`width: ${898 / totalSquares}px; height: ${470 / totalSquares}px;`
+	);
+	square.style.opacity = 0;
+	sketch.appendChild(square);
+};
+
 function createSquares(numberOfSquares) {
 	if (sketch.childNodes.length > 0) {
 		while (sketch.firstChild) {
@@ -10,16 +21,12 @@ function createSquares(numberOfSquares) {
 	}
 	let createdSquares = 0;
 	while (createdSquares < numberOfSquares * numberOfSquares) {
-		let square = document.createElement("div");
-		square.setAttribute("class", "square");
-		square.setAttribute(
-			"style",
-			`width: ${898 / numberOfSquares}px; height: ${468 / numberOfSquares}px`
-		);
-		sketch.appendChild(square);
+		newSquare(numberOfSquares);
 		createdSquares++;
 	}
 }
+
+document.addEventListener("DOMContentLoaded", () => createSquares(16));
 
 button.addEventListener("click", (ev) => {
 	ev.preventDefault();
